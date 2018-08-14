e5fee464a6d55b7024d9451e5524c9ed6bfbefca
florincy <simoneflorency@gmail.com>
2018-08-14 20:38:03 -0300

    Mapeamento do Cliente, v1

--- contrlole_de_gastos_poo/src/main/java/br/simoneflorincy/contrlole_de_gastos_poo/Cliente.java
+++ contrlole_de_gastos_poo/src/main/java/br/simoneflorincy/contrlole_de_gastos_poo/Cliente.java
@@ -5,23 +5,62 @@
  */
 package br.simoneflorincy.contrlole_de_gastos_poo;
 
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
 /**
  *
  * @author User
  */
-public class Cliente {
+@Entity
+@Table(name = "cliente")
+public class Cliente implements Serializable {
+
+    /**
+     * @return the cdCliente
+     */
+    public Integer getCdCliente() {
+        return cdCliente;
+    }
+
+    /**
+     * @param cdCliente the cdCliente to set
+     */
+    public void setCdCliente(Integer cdCliente) {
+        this.cdCliente = cdCliente;
+    }
+
+    /**
+     * @return the tp_visivel
+     */
+    public char getTp_visivel() {
+        return tp_visivel;
+    }
+
+    /**
+     * @param tp_visivel the tp_visivel to set
+     */
+    public void setTp_visivel(char tp_visivel) {
+        this.tp_visivel = tp_visivel;
+    }
 
     /**
      * @return the codIdentificacao
      */
-    public String getCodIdentificacao() {
+    public Integer getCodIdentificacao() {
         return codIdentificacao;
     }
 
     /**
      * @param codIdentificacao the codIdentificacao to set
      */
-    public void setCodIdentificacao(String codIdentificacao) {
+    public void setCodIdentificacao(Integer codIdentificacao) {
         this.codIdentificacao = codIdentificacao;
     }
 
@@ -94,11 +133,33 @@ public class Cliente {
     public void setEnderecoDoCliente(Endereco enderecoDoCliente) {
         this.enderecoDoCliente = enderecoDoCliente;
     }
-   private String codIdentificacao;
-   private String email;
-   private String nomeFantasia;
-   private String razaoSocial;
-   private String tipoCliente; 
-   private Endereco enderecoDoCliente;
-    
+    /*
+    cd_cliente integer NOT NULL,
+  endereco_cd_endereco integer,
+  nm_razaosocial character varying(100),
+  nm_fantasia character varying(25),
+  ds_email character varying(20),
+  cod_identificacao integer,
+  tipo_cliente character varying(60),
+  tp_visivel character(1),
+     */
+    @Column(name = "cod_identificacao")
+    private Integer codIdentificacao;
+    @Column(name = "ds_email")
+    private String email;
+    @Column(name = "nm_fantasia")
+    private String nomeFantasia;
+    @Column(name = "nm_razaosocial")
+    private String razaoSocial;
+    @Column(name = "tipo_cliente")
+    private String tipoCliente;
+    @Column(name = "endereco_cd_endereco")
+    @OneToOne(cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
+    private Endereco enderecoDoCliente;
+    @Column(name = "cd_cliente")
+    @Id
+    private Integer cdCliente;
+    @Column
+    private char tp_visivel;
+
 }
