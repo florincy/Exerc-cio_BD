2328abebc1da4c67a70b2c09b463fd4c85d8139f
florincy <simoneflorency@gmail.com>
2018-08-15 12:28:03 -0300

    Trabalho finalizado, 12:26

--- contrlole_de_gastos_poo/src/main/java/br/simoneflorincy/contrlole_de_gastos_poo/OrdemServico.java
+++ contrlole_de_gastos_poo/src/main/java/br/simoneflorincy/contrlole_de_gastos_poo/OrdemServico.java
@@ -5,13 +5,33 @@
  */
 package br.simoneflorincy.contrlole_de_gastos_poo;
 
+import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
 
-/**
- *
- * @author User
- */
-public class OrdemServico {
+@Entity
+@Table(name = "ordem_servico")
+public class OrdemServico implements Serializable {
+
+    /**
+     * @return the bool_fechado
+     */
+    public boolean isBool_fechado() {
+        return bool_fechado;
+    }
+
+    /**
+     * @param bool_fechado the bool_fechado to set
+     */
+    public void setBool_fechado(boolean bool_fechado) {
+        this.bool_fechado = bool_fechado;
+    }
 
     /**
      * @return the data
@@ -68,11 +88,26 @@ public class OrdemServico {
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
     }
+    @Column(name = "dt_ordem_servico")
     private Date data;
+    @Column(name = "vl_total")
     private double valorTotal;
+    @Column(name = "funcionario_cd_funcionario")
+    @OneToMany(fetch = FetchType.EAGER)
     private Funcionario funcionario;
+    @Column(name = " cliente_cd_cliente")
+    @OneToMany(fetch = FetchType.EAGER)
     private Cliente cliente;
-    
-    
-    
+    @Column
+    private boolean bool_fechado;
+    @Column
+    @Id
+    Integer cd_ordem_servico;
+    /* cd_ordem_servico integer NOT NULL,
+  funcionario_cd_funcionario integer NOT NULL,
+  cliente_cd_cliente integer,
+  dt_ordem_servico date,
+  vl_total numeric(100,2),
+  bool_fechado boolean,
+     */
 }
